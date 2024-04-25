c4d48cae84e089b3451021bedd80051bff438c59
Seth Derrick <seth@sethderrick.tech>
2024-04-25 08:09:55 -0500

    Remove extra paren preventing build

--- backend/functions/openai_requests.py
+++ backend/functions/openai_requests.py
@@ -9,7 +9,7 @@ openai.api_key = config("OPEN_AI_KEY")
 
  # Open AI - Whisper
  # Convert audio to text
-def convert_audio_to_text(audio_file)):
+def convert_audio_to_text(audio_file):
   try: 
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     message_text = transcript["text"]
