94419b2f8431e5ddbe6a71ae79a0fe812ef0c2d2
Seth Derrick <seth@sethderrick.tech>
2024-04-26 11:32:36 -0500

    Create Recorder container in Controller

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -13,7 +13,12 @@ function Controller() {
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={setMessages} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
-        Placeholder
+        {/* Recorder */}
+        <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500">
+          <div className="flex justify-center items-center w-full">
+            <div>Recorder</div>
+          </div>
+        </div>
       </div>
     </div>
   );
