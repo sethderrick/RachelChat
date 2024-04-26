64ecb4354c4abcd0d439c2fdb9e5eaa93a6c922f
Seth Derrick <seth@sethderrick.tech>
2024-04-26 13:45:54 -0500

    Tweak UI for better UX

--- frontend/src/components/Controller.tsx
+++ frontend/src/components/Controller.tsx
@@ -25,40 +25,88 @@ function Controller() {
     fetch(blobUrl)
       .then((res) => res.blob())
       .then(async (blob) => {
-        // Construct audio 
+        // Construct audio
         const formData = new FormData();
         formData.append("file", blob, "myFile.wav");
 
         // Send form data to API endpoint
-        await axios.post("http://127.0.0.1:8000/post-audio", formData, {
-          headers: { "Content-Type": "audio/mpeg" }, 
-          responseType: "arraybuffer",
-        })
-        .then((res: any) => {
-          const blob = res.data;
-          const audio = new Audio();
-          audio.src = createBlobUrl(blob);
+        await axios
+          .post("http://127.0.0.1:8000/post-audio", formData, {
+            headers: { "Content-Type": "audio/mpeg" },
+            responseType: "arraybuffer",
+          })
+          .then((res: any) => {
+            const blob = res.data;
+            const audio = new Audio();
+            audio.src = createBlobUrl(blob);
 
-          // Append to audio
-          const rachelMessage = { sender: "rachel", blobUrl: audio.src };
-          messagesArr.push(rachelMessage);
-          setMessages(messagesArr);
+            // Append to audio
+            const rachelMessage = { sender: "rachel", blobUrl: audio.src };
+            messagesArr.push(rachelMessage);
+            setMessages(messagesArr);
 
-          // Play audio
-          setIsLoading(false)
-          audio.play();
-        })
-        .catch((err) => {
-          console.error(err);
-          setIsLoading(false);
-        })
-    });
+            // Play audio
+            setIsLoading(false);
+            audio.play();
+          })
+          .catch((err) => {
+            console.error(err);
+            setIsLoading(false);
+          });
+      });
   };
 
   return (
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={setMessages} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+        {/* Conversation */}
+        <div className="mt-5 px-5">
+          {messages.map((audio, index) => {
+            return (
+              <div
+                key={index + audio.sender}
+                className={
+                  "flex flex-col " +
+                  (audio.sender == "rachel" && "flex items-end")
+                }
+              >
+                {/* Sender */}
+                <div className="mt-4">
+                  <p
+                    className={
+                      audio.sender == "rachel"
+                        ? "text-right mr-2 italic text-green-500"
+                        : "ml-2 italic text-blue-500"
+                    }
+                  >
+                    {audio.sender}
+                  </p>
+
+                  {/* Audio Message  */}
+                  <audio
+                    src={audio.blobUrl}
+                    className="appearance-none"
+                    controls
+                  />
+                </div>
+              </div>
+            );
+          })}
+
+          {messages.length == 0 && !isLoading && (
+            <div className="text-center font-light italic mt-10">
+              Send Rachel a message...
+            </div>
+          )}
+
+          {isLoading && (
+            <div className="text-center font-light italic mt-10 animate-pulse">
+              Give me a few seconds...
+            </div>
+          )}
+        </div>
+
         {/* Recorder */}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-sky-500 to-green-500">
           <div className="flex justify-center items-center w-full">
