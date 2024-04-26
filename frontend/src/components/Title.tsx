8fc3479ea1121f6800cb05fdf78b3cfc6a9262ea
Seth Derrick <seth@sethderrick.tech>
2024-04-26 11:28:31 -0500

    Complete Title component Reset render

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -28,8 +28,29 @@ function Title({ setMessages }: Props) {
     setIsResetting(false);
   };
   return (
-    <div>
-      <button onClick={resetConversation} className="bg-indigo-500 p-5">RESET</button>
+    <div className="flex justify-between items-center w-full p-4 bg-gray-900 text-white font-bold shadow">
+      <div className="italic">Rachel</div>
+      <button
+        onClick={resetConversation}
+        className={`transition-all duration-300 text-blue-300 hover:text-pink-500 ${
+          isResetting ? "animate-pulse" : ""
+        }`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
